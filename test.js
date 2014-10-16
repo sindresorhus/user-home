ab23b9ba99474e22b8bfa29cebbbc57c7eaa2627
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-16 14:32:55 +0200

    improve tests

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 'use strict';
+var fs = require('fs');
 var test = require('ava');
 var userHome = require('./');
 
 test(function (t) {
 	t.assert(userHome.length > 0);
+	t.assert(fs.existsSync(userHome));
+	t.end();
 });
