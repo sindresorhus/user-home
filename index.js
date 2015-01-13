8c773fdaf6239bbaf786737a0c34ece4180bb468
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-14 01:55:33 +0800

    remove moot fallbacks

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var user = env.LOGNAME || env.USER || env.LNAME || env.USERNAME;
 if (process.platform === 'win32') {
 	module.exports = env.USERPROFILE || env.HOMEDRIVE + env.HOMEPATH || home || null;
 } else if (process.platform === 'darwin') {
-	module.exports = home || (user ? '/Users/' + user : null) || null;
+	module.exports = home || (user ? '/Users/' + user : null);
 } else if (process.platform === 'linux') {
 	module.exports = home ||
-		(user ? (process.getuid() === 0 ? '/root' : '/home/' + user) : null) || null;
+		(user ? (process.getuid() === 0 ? '/root' : '/home/' + user) : null);
 } else {
 	module.exports = home || null;
 }
